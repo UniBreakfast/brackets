f655bc30bc99194aec5bc5854c714549c3a0698a
unibreakfast <unibreakfast1@gmail.com>
2020-03-03 11:59:25 +0200

    feat: add bracket pairs validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = bracketsConfig
+    .reduce((pairs, [open, close])=> Object.assign(pairs, {[open]: close}), {})
+  let queue = ''
+  for (const char of str) {
+    if (queue.slice(-1) == char) queue = queue.slice(0, -1)
+    else if (char in pairs) queue += pairs[char]
+    else return false
+  }
+  return !queue
 }
